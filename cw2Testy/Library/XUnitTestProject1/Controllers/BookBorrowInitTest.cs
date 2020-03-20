f695d9caf06880e548caba2dffe925bd9163918f
Jakub Parteka <JLV@kmd.dk>
2020-03-20 20:55:17 +0100

    Testy bez unit

---
+++ cw2Testy/Library/XUnitTestProject1/Controllers/BookBorrowInitTest.cs
@@ -0,0 +1,92 @@
+﻿using Library.Entities;
+using Library.Models.DTO;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTestProject1.Controllers
+{
+    public class BookBorrowInitTest
+    {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        public BookBorrowInitTest()
+        {
+            _server = ServerFactory.GetServerInstance();
+            _client = _server.CreateClient();
+
+
+            using (var scope = _server.Host.Services.CreateScope())
+            {
+                var _db = scope.ServiceProvider.GetRequiredService<LibraryContext>();
+
+                _db.BookBorrow.Add(new BookBorrow
+                {
+                    IdBookBorrow = 2,
+                    IdBook = 5,
+                    IdUser = 5,
+                    Comments = "None"
+                });
+
+                _db.SaveChanges();
+            }
+        }
+
+        [Fact]
+        public async Task PostBookBorrow_201CreatedAtRoute()
+        {
+            //Arrange i Act
+            var newBook = new BookBorrowDto
+            {
+                IdUser = 1,
+                IdBook = 1,
+                Comment = "None"
+            };
+
+            HttpContent stringContent = new StringContent(JsonConvert.SerializeObject(newBook), Encoding.UTF8, "application/json");
+            var httpResponse = await _client.PostAsync($"{_client.BaseAddress.AbsoluteUri}api/book-borrows", stringContent);
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            var content = await httpResponse.Content.ReadAsStringAsync();
+            var responseBook = JsonConvert.DeserializeObject<BookBorrow>(content);
+
+            Assert.True(responseBook.IdUser == 1);
+            Assert.True(responseBook.IdBook == 1);
+        }
+
+        [Fact]
+        public async Task PutBookBorrow_200Ok()
+        {
+
+            //Arrange i Act
+            var updatedBook = new UpdateBookBorrowDto
+            {
+                IdBookBorrow = 2,
+                IdUser = 5,
+                IdBook = 5,
+                DateFrom = new DateTime(),
+                DateTo = new DateTime(),
+                Comments ="none"
+            };
+
+            HttpContent stringContent = new StringContent(JsonConvert.SerializeObject(updatedBook), Encoding.UTF8, "application/json");
+            var httpResponse = await _client.PutAsync($"{_client.BaseAddress.AbsoluteUri}api/book-borrows/{updatedBook.IdBookBorrow}", stringContent);
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            var content = await httpResponse.Content.ReadAsStringAsync();
+            var responseBook = JsonConvert.DeserializeObject<Boolean>(content);
+            
+            Assert.True(responseBook);
+
+        }
+    }
+}
---
+++ cw2Testy/Library/XUnitTestProject1/Controllers/UsersUnitTests.cs
@@ -0,0 +1,97 @@
+﻿using Library.Controllers;
+using Library.Entities;
+using Library.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using System.Linq;
+using Library.Models.DTO;
+using Microsoft.AspNet.Identity;
+using Library.Helpers;
+
+namespace XUnitTestProject1.Controllers
+{
+    public class UsersUnitTests
+    {
+        [Fact]
+        public async Task GetUsers_200Ok()
+        {
+            //AAA
+            //Arrange
+            var m = new Mock<IUserRepository>();
+            ICollection<User> users = new List<User>
+            {
+                new User{IdUser=1, Name="kowalski", Email="kowalski@wp.pl"},
+                new User{IdUser=2, Name="kowalski2", Email="kowalski2@wp.pl"},
+                new User{IdUser=3, Name="kowalski3", Email="kowalski3@wp.pl"}
+            };
+
+            m.Setup(c => c.GetUsers()).Returns(Task.FromResult(users));
+
+            var controller = new UsersController(m.Object);
+
+            //Act
+            var result = await controller.GetUsers();
+
+            //Assert
+            Assert.True(result is OkObjectResult);
+            var r = result as OkObjectResult;
+            Assert.True((r.Value as ICollection<User>).Count == 3);
+            Assert.True((r.Value as ICollection<User>).ElementAt(0).Name == "kowalski");
+        }
+
+        [Fact]
+        public async Task AddUser_201CreatedAtRoute()
+        {
+            //AAA
+            //Arrange
+            var m = new Mock<IUserRepository>();
+            UserDto userDto = new UserDto { Login = "Kowal", Name = "kowalski", Email = "kowalski@wp.pl", Password = "Legia" };
+            User user = new User
+            {
+                Name = userDto.Name,
+                Email = userDto.Email,
+                Login = userDto.Login,
+                Surname = userDto.Surname,
+                Password = new PasswordHasher().HashPassword(userDto.Password),
+                IdUserRoleDict = (int)UserRoleHelper.UserRolesEnum.Reader
+            };
+
+            m.Setup(c => c.AddUser(userDto)).Returns(Task.FromResult(user));
+            var controller = new UsersController(m.Object);
+
+            //Act
+            var result = await controller.AddUser(userDto);
+
+            //Assert
+            Assert.True(result is CreatedAtRouteResult);
+            var r = result as CreatedAtRouteResult;
+            Assert.True((r.Value as User).Email == "kowalski@wp.pl");
+        }
+
+        [Fact]
+        public async Task GetUser_200Ok()
+        {
+            //AAA
+            //Arrange
+            var m = new Mock<IUserRepository>();
+
+            var user = new User { IdUser = 1, Name = "kowalski", Email = "kowalski@wp.pl" };
+
+            m.Setup(c => c.GetUser(user.IdUser)).Returns(Task.FromResult(user));
+            var controller = new UsersController(m.Object);
+
+            //Act
+            var result = await controller.GetUser(user.IdUser);
+
+            //Assert
+            Assert.True(result is OkObjectResult);
+            var r = result as OkObjectResult;
+            Assert.True((r.Value as User).Name == "kowalski");
+        }
+    }
+}
